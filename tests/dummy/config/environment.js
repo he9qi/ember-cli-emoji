63ea90f0a016e7a7b5e2af2e4618efc51253f7ba
Qi He <qhe@heyook.com>
2015-07-30 20:33:39 -0700

    remove view lookup log

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -35,10 +35,6 @@ module.exports = function(environment) {
     ENV.baseURL = '/';
     ENV.locationType = 'none';
 
-    // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
-
     ENV.APP.rootElement = '#ember-testing';
   }
 
